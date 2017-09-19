676be88f5b4eb4e07daf893cb2a7fea8be7db562
Kenneth Tso <iamkennytso@gmail.com>
2017-09-19 09:59:45 -0400

    (feat) Establish Database Connection.

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const MongoClient = require('mongodb')
 
 mongoose.connect(`mongodb://testuser:testtest@ds135534.mlab.com:35534/binarybandicoots`, (err, win) => {
-	if(err){
-		console.error(`Error connecting: ${err}`)
-	} else {
-		console.log(`(>'.')> Database Connected <('.'<)`) 
-	}
+  if (err) {
+    console.error(`Error connecting: ${err}`)
+  } else {
+    console.log(`(>'.')> Database Connected <('.'<)`) 
+  }
 });
 
 var db = mongoose.connection;

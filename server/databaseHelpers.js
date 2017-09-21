35c74cea483735acccc9458889967b70be9b4412
Craig Gordon <cyghfer@gmail.com>
2017-09-21 17:49:47 -0400

    Abstract database helper functions to separate file

---
+++ server/databaseHelpers.js
@@ -0,0 +1,41 @@
+const Item = require('../db/itemModel.js');
+const Event = require('../db/eventModel.js');
+const Account = require('../db/accountModel.js');
+
+exports.createNewEvent = (eventName = '', tipRate, taxRate, discountRaw, discountRate) => {
+  return Event.create({ eventName, tipRate, taxRate, discountRaw, discountRate });
+};
+
+exports.getEvent = (eventID) => {
+  return Event.find({ _id: eventID });
+};
+
+exports.getAccountByPhoneNumber = (phoneNumber) => {
+  return Account.findOne({ phoneNumber });
+};
+
+exports.getAccountById = (id) => {
+  return Account.findOne({ _id: id });
+};
+
+exports.createOrUpdateAccount = (phoneNumber, eventID) => {
+  let query = { phoneNumber };
+  let update = { $push: { events: eventID } };
+  let options = { new: true, upsert: true };
+  return Account.findOneAndUpdate(query, update, options);
+};
+
+exports.createNewItem = (itemName, quantity = 1, price, eventID) => {
+  return Item.create({ eventID, itemName, quantity, price });
+};
+
+exports.getItems = (eventID) => {
+  return Item.find({ eventID });
+};
+
+exports.updateAndReturnItem = (item, diner) => {
+  let query = { _id: item };
+  let update = { $push: { shares: diner } };
+  let options = { new: true };
+  return Item.findOneAndUpdate(query, update, options);
+};

95eede20691e1e30414ffa458b9cebec8255bccc
Craig Gordon <cyghfer@gmail.com>
2017-09-21 14:45:13 -0400

    Clean up back-end files + add clarifying comments

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,16 +8,19 @@ const db = require('../db/db.js');
 const Item = require('../db/itemModel.js');
 const Event = require('../db/eventModel.js');
 const Account = require('../db/accountModel.js');
-const algorithm = require('./kennysMagicalAlgorithm.js');
+const algorithm = require('./calculations.js');
 const axios = require('axios');
 const htmlMiner = require('html-miner');
+require('dotenv').config();
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(cors());
 
+// This route sends the GET request to Google's menu page partner site
+// req.body.restaurant is determined by the Organizer's selection in the Google Maps search
+
 app.post('/restaurant', (req, res) => {
-  console.log(req.body.restaurant)
   axios.get(`http://places.singleplatform.com/${req.body.restaurant}/menu?ref=google`)
     .then(html => {
       let menuData = htmlMiner(html.data, {
@@ -31,8 +33,10 @@ app.post('/restaurant', (req, res) => {
       menuData = menuData.itemList.filter(obj => obj.hasOwnProperty('price'));
       res.send(menuData);
     })
-    .catch(err => res.send('Error retrieving from restaurant page:', err))
+    .catch(err => res.send('Error retrieving from restaurant page:', err));
 });
+
+// This route is the main Data Receiver -- we insert new Documents into the Events, Accounts, and Items tables
 
 app.post('/meals', (req, res) => {
   // We are expecting in req.body:
@@ -41,7 +45,7 @@ app.post('/meals', (req, res) => {
   //   phoneNumbers: [ phoneNumber1, phoneNumber2, ... ] }
 
   // This variable will get set in the first .then block, just after creating the new Event mongoose document
-  var currentEventID;
+  let currentEventID;
   // We first create a new Event document in order to assign the generated Primary Key to currentEventID
   // If an Event Name was specified by the Organizer, use that; otherwise use an empty string
   Event.create({
@@ -58,7 +62,6 @@ app.post('/meals', (req, res) => {
     // Now, for each phone number, we either create a new Account and enter it into the DB, or we update the existing entry
     // Every Account entry has an associated phoneNumber and an array of all Events they have participated in
     return Promise.all(req.body.phoneNumbers.map(phoneNumber => {
-      console.log('currently mapping')
       let query = { phoneNumber };
       let update = { $push: { events: currentEventID } };
       let options = { new: true, upsert: true };
@@ -67,11 +70,7 @@ app.post('/meals', (req, res) => {
   })
   // Using currentEventID, insert each Item into the database
   .then(() => {
-    console.log('made it this far 1', currentEventID)
-    console.log('req.body', req.body)
-
     return Promise.all(req.body.receiptItems.map(item => {
-      console.log('mapping 2');
       return Item.create({
         eventID: currentEventID,
         itemName: item.item,
@@ -80,13 +79,13 @@ app.post('/meals', (req, res) => {
       });
     }))
   })
-  // When ALL items have been inserted (hence the uses of Promise.all), send currentEventID back to the client-side
   .then(() => {
-    console.log('made it this far 2, id=', currentEventID)
     res.send(200, currentEventID);
   })
   .catch(err => res.send(('Database insertion error:', err)));
 });
+
+// This route gets all the items in the Items table that have the eventID provided in the URL
 
 app.get('/meals*', (req, res) => {
   Item.find({ eventID: req.url.slice(7) })
@@ -94,11 +93,14 @@ app.get('/meals*', (req, res) => {
     .catch(err => res.send('Database retrieval error:', err));
 });
 
+// This route handles one POST request per Diner
+// It updates the shares array of each Item in the database with the appropriate Diner
+// Then it sends back the new Items for rendering the Results page
+
 app.post('/share', (req, res) => {
-  console.log('req.body /share', req.body);
   Promise.all(req.body.receiptItems.map(item => {
-    let query = { _id: item };
-    let update = { $push: { shares: req.body.diner } };
+    let query = { _id: item };                          // req.body.diner is the Diner's name --
+    let update = { $push: { shares: req.body.diner } }; // for the purposes of rendering the final receipt page
     let options = { new: true };
     return Item.findOneAndUpdate(query, update, options);
   }))
@@ -106,69 +108,55 @@ app.post('/share', (req, res) => {
     .catch(err => res.send('Database update error:', err));
 });
 
-app.post('/taxRate', (req,res) => {
-  console.log('hit server');
-  console.log(req.body);
-  var rate = getTaxRateLatLng(req.body.latlng);
-  res.send(rate);
-});
+// This route handles an individual Diner wanting to check their Events History
+// We find the Account in the database, then send a text message via Twillio
 
 app.post('/accounts', (req, res) => {
-  console.log('/accounts req.body:', req.body);
-  var number = '+1' + req.body.number;
+  let number = '+1' + req.body.number;
   Account.findOne({
     phoneNumber: number
   })
-  .then(acct => {
-    console.log('accout found or not??:', acct);
-    if (acct) {
-      console.log('acct', acct);
-      var acctId = acct._id;
-      console.log('acctId', acctId);
-      console.log('number', number);
-      Client.messages.create({
-        from: process.env.FROM,
-        to: number,
-        body: 'http://' + process.env.HOST + ':' + process.env.PORT + '/#!/account?' + acctId
-      })
-      .then(message => console.log('message sent', message))
-      .catch(err => console.log('err', err));
-      res.status(201).send('it worked');
-    } else {
-      res.status(400).send('error, you do not have an account somehow');
-    }  
-  })
-  .catch(err => res.status(400).send('error posting to /accounts', err));
+    .then(acct => {
+      if (acct) {
+        let acctId = acct._id;
+        Client.messages.create({
+          from: process.env.FROM,
+          to: number,
+          body: 'http://' + process.env.HOST + ':' + process.env.PORT + '/#!/account?' + acctId
+        })
+          .then(message => res.status(201).send('History sent'))
+          .catch(err => res.status(400).send('Error sending text message with history:', err));
+      } else {
+        res.status(400).send('Error: you do not have an account');
+      }  
+    })
+    .catch(err => res.status(400).send('Error finding Account in database:', err));
 })
+
+// This route calculates the Totals that each Diner owes, with Tax and Tip included
 
 app.get('/receipt*', (req, res) => {
   let currentItems;
   let event = req.url.slice(9);
-  //gets all the items corresponding to this event
-  Item.find({eventID: event})
+  Item.find({ eventID: event })
     .then(items => {
       currentItems = items;
-      //gets all the rates corrsponding to this event
-      return Event.find({_id: event})
+      return Event.find({ _id: event })
     })
     .then(event => {
-      var rateObject = algorithm.getRates(event);
-      let receiptTotals = algorithm.calculateTotals(currentItems, rateObject);
+      let eventRates = algorithm.getRates(event);
+      let receiptTotals = algorithm.calculateTotals(currentItems, eventRates);
       res.send(receiptTotals);
     })
 });
 
+// This route finds a Diner's history and sends back all events they have been involved in
+
 app.get('/history*', (req, res) => {
   let id = req.url.slice(9);
-  console.log('id', id);
   Account.findOne({ _id: id })
-    .then(account => {
-      console.log('account', account);
-      res.send(account.events);
-    })
-    .catch(err => {
-      res.send('Error finding Account in database:', err);
-    });
+    .then(account => res.send(account.events));
+    .catch(err => res.send('Error finding Account in database:', err));
 });
 
 module.exports = app;
